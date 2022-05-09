1ff9573dc050b1d2523ebeddcdc2c9d1a9b3e6ff
AfterWin <CJ_Zheng1023@hotmail.com>
2022-05-09 21:51:24 +0800

    refactor: 修改简历地址

--- src/layouts/footer/Index.vue
+++ src/layouts/footer/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <a-footer>
     <div class="footer">
-      <a href="https://cj-zheng1023.github.io/" target="_blank">&copy;个人简历</a>
+      <a href="https://cj-zheng1023.vercel.app/" target="_blank">&copy;个人简历</a>
     </div>
   </a-footer>
 </template>
